6761c428d84b0fca7d1d9cbc00353b32e5478980
Bibintanggg <bintangyudhaa8@gmail.com>
2025-11-12 08:50:02 +0700

    refactor: change sidebar and remove it

--- resources/js/layout.tsx
+++ resources/js/layout.tsx
@@ -1,5 +1,5 @@
 import { SidebarProvider, SidebarTrigger } from "@/Components/ui/sidebar"
-import AppSidebar from "@/Components/app-sidebar"
+import {AppSidebar} from "@/Components/sidebar/app-sidebar"
 
 export default function SidebarLayout({ children }: { children: React.ReactNode }) {
   return (
